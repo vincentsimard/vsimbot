c0abdb3599aeef6e97fc03b26ef8572c043fbd39
Vincent Simard <vincent@vincentsimard.com>
2014-02-19 17:15:30 -0500

    Making halfmove clock and fullmove number optional for fen format

--- src/EventHandlers.js
+++ src/EventHandlers.js
@@ -8,13 +8,31 @@ var bot, config;
 // @TODO: This is a very simplistic pgn regexp. No support for variations or comments
 var pgnRENumber = "\\d+\\.\\s*";
 var pgnREPly = "[\\w\\+\\-#=]{2,8}\\s*";
+var pgnREOnePly = "(" + pgnRENumber + pgnREPly + ")";
 var pgnRETwoPlys = "(" + pgnRENumber + pgnREPly + pgnREPly + "\\s*)";
-var pgnREHeader = "(\\[([^\\]]+)\\]\\s*)";
+var pgnREHeader = "(\\[([^\\]]+)\\]\\s*)*";
 var pgnREResult = "(\\*|1\\-0|0\\-1|0\\.5\\-0\\.5|\\.5\\-\\.5|1\\/2\\-1\\/2)?";
-var pgnRE = "(" + pgnREHeader + "*" + pgnRETwoPlys + "{2,}\\s*(" + pgnRENumber + pgnREPly + ")?" + pgnREResult + ")";
+var pgnRE = "(" +
+  pgnREHeader +
+  pgnRETwoPlys + "{2,}\\s*" +
+  pgnREOnePly + "?" +
+  pgnREResult +
+  ")";
 
 // Thanks to http://chess.stackexchange.com/questions/1482/how-to-know-when-a-fen-position-is-legal
-var fenRE = "([rnbqkpRNBQKP1-8]+\\/){7}([rnbqkpRNBQKP1-8]+)\\s[bw-]\\s(([a-hkqA-HKQ]{1,4})|(-))\\s(([a-h][36])|(-))\\s\\d+\\s\\d+";
+var fenREPiecePlacement = "([rnbqkpRNBQKP1-8]+\\/){7}([rnbqkpRNBQKP1-8]+)";
+var fenREActiveColor = "[bw-]";
+var fenRECastlingAvailability = "(([a-hkqA-HKQ]{1,4})|(-))";
+var fenREEnPassantTarget = "(([a-h][36])|(-))";
+var fenREHalfMoveClock = "\\d*";
+var fenREFullMoveNumber = "\\d*";
+var fenRE = "" +
+  fenREPiecePlacement + "\\s" +
+  fenREActiveColor + "\\s" +
+  fenRECastlingAvailability + "\\s" +
+  fenREEnPassantTarget + "\\s?" +
+  fenREHalfMoveClock + "\\s?" +
+  fenREFullMoveNumber;
 
 var patterns = {};
 
