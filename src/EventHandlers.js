7978292dfb548b8db78e1fbb0bc756827a9a481f
Vincent Simard <vincent@vincentsimard.com>
2014-02-22 18:20:56 -0500

    Created the /eval command. Everything is a mess

--- src/EventHandlers.js
+++ src/EventHandlers.js
@@ -36,6 +36,8 @@ var fenRE = "" +
   fenREHalfMoveClock + "\\s?" +
   fenREFullMoveNumber;
 
+var evalfenRE = "(eval|evaluate|analyze|score)\\s(" + fenRE + ")";
+
 var patterns = {};
 
 // finger icc
@@ -105,10 +107,78 @@ patterns[pgnRE] = function(from, to, message, raw, match) {
   addToChessPasteBin(from, to, message, raw, match);
 };
 
+// evaluate position from fen
+// @TODO: This is a mess
+// @TODO: Show recommended move?
+patterns[evalfenRE] = function(from, to, message, raw, match) {
+  var util = require('util');
+  var spawn = require('child_process').spawn;
+  var crafty = spawn('crafty');
+  var dataCache = [];
+  var fen = match[2];
+  var text = '';
+
+  console.message('/eval %s'.input, to, from, fen);
+
+  crafty.stderr.on('data', function (data) {
+    // console.log('stderr: ' + data);
+  });
+
+  crafty.stdout.on('data', function (data) {
+    dataCache.push(data);
+  });
+
+  crafty.on('exit', function (code) {
+    var i = dataCache.length;
+
+    var line, match;
+
+    var depth, mates, mated;
+    var values, time, score;
+
+    while (i--) {
+      line = dataCache[i] + '';
+      match = line.match(/^(\d+)\->|(mated?\sin\s\d+\smoves?)|((\d\-\d)\s\{(White|Black)\smates\})/i);
+
+      if (match) { break; }
+    }
+
+    if (!match) { return; }
+
+    depth = match[1];
+    mates = match[2];
+    mated = match[3];
+
+    values = line.match(/\S+/ig);
+    time  = values[1];
+    score = values[2];
+
+    if (depth) { text += score + ' (time=' + time + 's, depth=' + depth + ')'; }
+    if (mates) { text += mates; } // @TODO: Specify which color mates or gets mated
+    if (mated) { text += mated; }
+
+    notifyChannelAndLogMessage(to, text);
+  });
+
+  // @TODO: Moves these options to a config file?
+  crafty.stdin.write('log off\n');
+  crafty.stdin.write('ponder off\n');
+  crafty.stdin.write('st 30\n');
+  // crafty.stdin.write('sd 18\n');
+  // crafty.stdin.write('book off\n');
+
+  crafty.stdin.write('setboard ' + fen + '\n');
+
+  // crafty.stdin.write('score\n');
+  crafty.stdin.write('go\n');
+  crafty.stdin.write('quit\n');
+};
+
 // post fen to chesspastebin
 patterns[fenRE] = function(from, to, message, raw, match) {
   addToChessPasteBin(from, to, message, raw, match, true);
 };
+
 
 
 
