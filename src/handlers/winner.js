a5d23c6378fda9037780130b62e6df41fb300191
Vincent Simard <vincent@vincentsimard.com>
2014-05-10 00:49:35 -0400

    Adding winner command for #amazingoid

---
+++ src/handlers/winner.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var nconf = require('nconf');
+
+var winner = function(from, to, message, raw, match) {
+  if (to !== "#amazingoid") { return; }
+  if (!(from === nconf.get('operator') || '#' + from === to)) { return; }
+
+  console.message("/!winner".input, to, from);
+
+  console.say(to, "/me And the winner is...");
+  console.say(to, "/me !!! amazingoid !!! (Amazingoid does not follow this channel.)");
+};
+
+
+
+module.exports.event = "message#";
+module.exports.pattern = "^!winner";
+module.exports.handler = winner;
