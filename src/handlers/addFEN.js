bef732a0327620e6d91c4a5e2d3d3eecfc38caa8
Thibault Duplessis <thibault.duplessis@gmail.com>
2015-03-24 18:11:42 +0100

    fix addFen (use lichess.org ;)

--- src/handlers/addFEN.js
+++ src/handlers/addFEN.js
@@ -1,30 +1,15 @@
 'use strict';
 
-var nconf = require('nconf');
-var cpb = require('./../ChessPasteBin.js');
 var Utils = require('./../Utils.js');
 
-
-
-// post FEN to chesspastebin
+// post FEN to lichess.org
 var addFEN = function(from, to, message, raw, match) {
-  var key = nconf.get('chesspastebinAPIKey');
+
   var fen = match[0];
-  var sandbox = nconf.get('chesspastebinSandbox');
-
-  console.message('/chesspastebin.addFEN %s'.input, to, from, fen);
-
-  cpb.addFEN(key, fen, from, undefined, sandbox, function(id) {
-    if (isNaN(id)) { return; }
-
-    var url = 'http://www.chesspastebin.com/?p=' + id;
-    var text = 'The FEN posted by ' + from + ' is now available at: ' + url;
-
-    console.say(to, text);
-  });
+  var url = 'http://lichess.org/editor/' + fen;
+  var text = 'The FEN posted by ' + from + ' is now available at: ' + url;
+  console.say(to, text);
 };
-
-
 
 module.exports.event = 'message#';
 module.exports.pattern = Utils.fenRE;
